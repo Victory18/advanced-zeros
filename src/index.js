e629f471655e8e26db992a5ce1054a562c029a08
raccooccoon <zavadskaya_91@mail.ru>
2019-03-17 23:47:49 +0300

    3

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 module.exports = function getZerosCount(number, base) {
   function factorial() {
-    var withZeros = +result.toString(base);
-    var withoutZeros = +withZeros.toString().split('').reverse().join('');
-    var zeros = withZeros.toString().length - withoutZeros.toString().length;
     var result = 1;
     for (var i = 1; i <= number; i++) {
       result *= i;
     }
-    
-  return zeros;
+  return result.toString(base).toString().length - result.toString(base).toString().split('').reverse().join('').length;
   }
 }
