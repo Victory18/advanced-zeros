c866a78aefe6d73e0c6677d18f7c37ecf3623725
raccooccoon <zavadskaya_91@mail.ru>
2019-03-18 22:54:03 +0300

    8

--- src/index.js
+++ src/index.js
@@ -9,11 +9,7 @@ module.exports = function getZerosCount(number, base) {
   } while (number >= 0);
 
   var newNumber = +arr.reverse().join('');
-  var count = 0;
-
-  while (newNumber) {
-    newNumber = Math.floor(newNumber / 5) | 0;
-    count += newNumber;
-  }
+  var count = Math.floor(newNumber / 5);
+ 
 return count;
 }
