12ca1f63f10d5396d285154a4259399636fb689a
raccooccoon <zavadskaya_91@mail.ru>
2019-03-19 01:14:38 +0300

    1

--- src/index.js
+++ src/index.js
@@ -12,20 +12,20 @@ module.exports = function getZerosCount(number, base) {
  
  
 //return Math.floor(newNumber / 5);
-var fact = 1;
 var arr =[];
 var el = 0;
 var count = 0;
+var fact = 0;
 
 for (var i = 1; i <= number; i++) {
   fact = fact * i;
 }
 
 do {
-  el = fact % base;
+  el = Math.floor(fact) % base;
   arr.push(el);
-  fact = Math.floor(fact / base);
-  } while (fact != 0)
+  fact = fact / base;
+  } while (fact != 0);
 
 var newNumber = arr.reverse().join('');
 
